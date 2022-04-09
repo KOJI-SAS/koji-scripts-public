04c8fd722c98d2c58ee22e8dc2e1d3b9b2ff8bff
Tristan De Oliveira <tdeoliv.18@gmail.com>
2022-04-09 15:05:43 +0200

    Create setup-monorepo.sh

---
+++ setup-monorepo.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+PROJECT_NAME=$1
+
+reset_git() {
+  rm -rf .git
+  git init
+}
+
+git clone git@github.com:KOJI-SAS/koji-typescript-monorepo.git $PROJECT_NAME
+cd $_
+
+sh ./scripts/setup-github.sh
+
+while ! LC_ALL=C find ./ -type d -exec rename ''s/project-name/$PROJECT_NAME/'' {} ";" 2>/dev/null
+do
+  echo "Renaming directories"
+done
+echo "Renaming files"
+LC_ALL=C find ./ -type f -exec rename ''s/project-name/$PROJECT_NAME/'' {} ";" 
+
+echo "Replacing in files files"
+LC_ALL=C find ./ -type f -exec sed -i '' -e ''s/project-name/$PROJECT_NAME/g'' {} \;
+
+echo "Setting up Git"
+reset_git
+git add -A
+git commit -m "chore: import template and setup monorepo"
+git branch -M trunk
